ff08501ffef17f96542fcb15b9080c8d4cdd82bf
Nikolas Furuta <nikolasfuruta@gmail.com>
2022-04-26 02:30:31 -0300

    feat: add PaletasController

---
+++ src/controllers/paletas-controller.js
@@ -0,0 +1,64 @@
+const PaletasService = require('../services/paletas-service')
+
+const findAll = async (req, res) => {
+  try {
+    const result = await PaletasService.findAll
+    res.status(200).send(result)
+  } catch (err) {
+    console.error(err)
+    res.status(403).send({ message: 'Erro' })
+  }
+}
+
+const findOne = async (req, res) => {
+  const id = req.params.id
+  try {
+    const result = await PaletasService.findOne(id)
+    res.status(200).send(result)
+  } catch (err) {
+    console.error(err)
+    res.status(403).send({ message: 'Erro' })
+  }
+}
+
+const create = async (req, res) => {
+  const info = req.body
+  try {
+    const result = await PaletasService.create(info)
+    res.status(200).send(result)
+  } catch (err) {
+    console.error(err)
+    res.status(403).send({ message: 'Erro' })
+  }
+}
+
+const update = async (req, res) => {
+  const id = req.params.id
+  const info = req.body
+  try {
+    const result = await PaletasService.update(id, info)
+    res.status(200).send(result)
+  } catch (err) {
+    console.error(err)
+    res.status(403).send({ message: 'Erro' })
+  }
+}
+
+const remove = async (req, res) => {
+  const id = req.params.id
+  try {
+    const result = await PaletasService.remove(id)
+    res.status(200).send(result)
+  } catch (err) {
+    console.error(err)
+    res.status(403).send({ message: 'Erro' })
+  }
+}
+
+module.exports = {
+  findAll,
+  findOne,
+  create,
+  update,
+  remove,
+}
--- src/routes/paletas-routes.js
+++ src/routes/paletas-routes.js
@@ -1,14 +1,16 @@
-// const router = require('express').Router()
-// const { validId, validObjectBody } = require('../middlewares/paletas')
-// const PaletasController
+const router = require('express').Router()
+const { validId, validObjectBody } = require('../middlewares/paletas')
+const PaletasController = require('../controllers/paletas-controller')
 
-// router.route('/paletas')
-// .get(PaletasController.findAll)
-// .post(validObjectBody, PaletasController.add)
+router
+  .route('/paletas')
+  .get(PaletasController.findAll)
+  .post(validObjectBody, PaletasController.create)
 
-// router.route('/paletas/:id')
-// .get(validId, PaletasController.findOne)
-// .patch(validId, validObjectBody, PaletasController.update)
-// .delete(validId, PaletasController.delete)
+router
+  .route('/paletas/:id')
+  .get(validId, PaletasController.findOne)
+  .patch(validId, validObjectBody, PaletasController.update)
+  .delete(validId, PaletasController.remove)
 
-// module.exports = router
+module.exports = router
