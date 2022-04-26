f10dcd0e988bb4ec87ce4c6bc845ee767f7e8587
Nikolas Furuta <nikolasfuruta@gmail.com>
2022-04-26 02:14:33 -0300

    feat: add validation before PaletasController calls

--- src/routes/paletas-routes.js
+++ src/routes/paletas-routes.js
@@ -1,7 +1,14 @@
-const router = require('express').Router()
+// const router = require('express').Router()
+// const { validId, validObjectBody } = require('../middlewares/paletas')
+// const PaletasController
 
-router.route('/paletas').get().post()
+// router.route('/paletas')
+// .get(PaletasController.findAll)
+// .post(validObjectBody, PaletasController.add)
 
-router.route('/paletas/:id').get().patch().delete()
+// router.route('/paletas/:id')
+// .get(validId, PaletasController.findOne)
+// .patch(validId, validObjectBody, PaletasController.update)
+// .delete(validId, PaletasController.delete)
 
-module.exports = router
+// module.exports = router
