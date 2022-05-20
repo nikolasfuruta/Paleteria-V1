20a720493af98a565f9f6f64d773204d9f7b7763
Nikolas Furuta <nikolasfuruta@gmail.com>
2022-05-19 21:07:46 -0300

    chore: add PaleteriaV1

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const conectarBancoDeDados = require('./src/db/connection')
 const app = express()
 app.use(express.json())
 app.use(cors())
-app.use(route)
+app.use('/v1', route)
 
 const port = process.env.PORT || 3000
 
--- src/middlewares/paletas.js
+++ src/middlewares/paletas.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const validId = (req, res, next) => {
   const id = req.params.id
   if (!mongoose.Types.ObjectId.isValid(id)) {
-    return res.status(400).send({ message: 'ID inválido' })
+    throw new Error('ID inválido')
   }
   next()
 }
@@ -11,7 +11,7 @@ const validId = (req, res, next) => {
 const validObjectBody = (req, res, next) => {
   const info = req.body
   if (!info || !info.sabor || !info.descricao || !info.foto || !info.preco) {
-    return res.status(400).send({ message: 'Erro na informação passada' })
+    throw new Error('Erro na informação passada')
   }
   next()
 }
